ef934c4957776137e6dacdd43d27a4c7f3113b37
marko0418 <markonikolic0418@gmail.com>
2022-04-22 12:52:52 +0200

    ciaoc

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stu
 		
 		char operator;
 		BigDecimal number1, number2, result1 = BigDecimal.ZERO;
